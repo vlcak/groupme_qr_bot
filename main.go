02101cb578884d4f1d00cba145e965f1ebbeed3d
Jakub Vlcek <javlcek@microsoft.com>
2023-05-16 23:46:52 +0200

    fix cron

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ func main() {
 
 	cronWorker := NewCronWorker(bankChecker, sheetOperator, messageService)
 	c := cron.New()
-	c.AddFunc("/15 * * * * *", func() { cronWorker.CheckNewPayments() })
+	c.AddFunc("*/15 * * * * *", func() { cronWorker.CheckNewPayments() })
 
 	handler := NewHandler(imageService, messageService, tymujClient, sheetOperator, *flagBotID, *flagDbURL)
 	fmt.Printf("Starting server...\n")
